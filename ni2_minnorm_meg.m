cb2ce7195b1136034566cd7850daf7169e67062b
johanna-zumer <johanna.zumer@gmail.com>
2013-04-25 09:04:54 +0200

    update for minnorm homework

--- ni2_minnorm_meg.m
+++ ni2_minnorm_meg.m
@@ -1,5 +1,207 @@
 % Minimum norm estimate for source localization
 
+%% non-uniqueness in underdetermined system
+L=[1 0 1;  0 1 1];
+
+y=[2 1]';
+
+% 2 = x +     z
+% 1 =     y + z
+% For any z, we can find x and y that solve the system
+
+% Try z = 0
+% x=2, y=1;
+
+% Try z = 3;
+% x=-1, y = -2
+
+% Try for yourself two more values of z.  Can you always find a set of x
+% and y that solves the equations?
+
+% Try z = 2;
+% x=0, y = -1
+
+snorm1=sqrt(s(1)^2+s(2)^2+s(3)^2)
+snorm2=sqrt(sum(s(:).^2))
+snorm3=norm(s)
+
+%three test solutions are:
+
+s=[2 1 0];
+norm(s)
+s=[-1 -2 3];
+norm(s)
+s=[0 -1 2];
+norm(s)
+
+% pinv
+
+Lpinv=L'*(L*L')^-1
+Lpinv=pinv(L)
+
+Lpinv1=L\eye(2)
+
+% Now try pinv
+s_mn=Lpinv*[2 1]'
+% [1 0 1]
+lf*smn
+
+%% Null space of leadfield
+
+% 2 sources, one radial, one tangential (MEG sphere)
+% see that if dot of source to LF is null, then can never recover.
+
+L=[1 0 1;  0 1 1];
+
+s1=[2 1 0]';
+s2=[-1 -2 3]';
+
+L*s1+L*s2
+s3=[1 1 -1]';
+
+L*(s1+s3)
+
+
+%% tikhonv regularisation and sensor noise
+
+% add sensor noise
+n=[.1 -.3]'
+y_n=y+n
+
+Lpinv=L'*(L*L')^-1
+
+Lpinv*y_n
+norm(Lpinv*y_n)
+
+Rn=diag(diag(n*n'))
+% Rn=n*n'
+
+Lpinv_reg=L'*(L*L'+Rn)^-1;
+Lpinv_reg*y_n;
+norm(Lpinv_reg*y_n)
+
+Lpinv_reg=L'*(L*L'+2*Rn)^-1;
+Lpinv_reg*y_n;
+norm(Lpinv_reg*y_n)
+
+Lpinv_reg=L'*(L*L'+200*Rn)^-1;
+Lpinv_reg*y_n;
+norm(Lpinv_reg*y_n)
+
+Lpinv_reg=L'*(L*L'+2e15*Rn)^-1;
+Lpinv_reg*y_n;
+norm(Lpinv_reg*y_n)
+
+
+%% effect of column norm (depth weighting)
+
+L=[4 0 1;  0 4 1];
+y=[1 1]';
+
+Lpinv=L'*(L*L')^-1;
+s_mn=Lpinv*y
+
+% compute colnorm
+Lcolnorm=sqrt(sum(L.^2,1))
+% Lcolnorm=sum(L.^2,1)
+
+% R_s=diag(Lcolnorm.^2)
+R_s=diag(Lcolnorm)
+
+Lpinv_dw=inv(R_s)*L'*(L*inv(R_s)*L')^-1
+
+Lpinv_dw*y
+
+L_normed=L./repmat(Lcolnorm,2,1)
+
+pinv(L_normed)*y
+
+[L./repmat(nut_colnorm(L),2,1)]\eye(2)*y
+
+
+
+
+%% First some matrix inversion
+% 
+% % Invert square matrix
+% a=diag([1 2 3 4])
+% inv(a)
+% 
+% b=[1 2;3 4]
+% inv(b)
+% 
+% 1/[b(1,1)*b(2,2)-b(1,2)*b(2,1)] * [b(2,2) -b(1,2); -b(2,1) b(1,1)]
+% 
+% % Can't invert non-square
+% c=[1 2 3; 4 5 6]
+% inv(c)
+% 
+% 
+% % Verify that pinv is l'*inv(l*l')
+% ci=pinv(c);
+% ci*c
+% c*ci
+% 
+% d=[1 2; 3 4; 5 6]
+% di=pinv(d)
+% 
+% % is pinv still equal to l'*inv(l*l')
+% 
+% % what happens if lf not independent rows?
+% e=[1 10;3 30]
+% e=[1 10;3 30.0001]
+% 
+% e(1,:)=[1 4 5];
+% e(2,:)=[2 -4 -3];
+% e(3,:)=2*e(1,:) + 5*e(2,:);
+
+
+
+
+%% apply to simulations
+
+
+
+% 2 sources plus heartbeat from outside 
+% (component of heartbeat with LF will project in)
+% assumption that all activity must come from within Lc
+
+
+% resolution matrix
+% s_est = w * L * s
+% source only at s2, what is value at s1?
+
+%% minimization algos
+
+% % show how source prior weights importance of that error minimization more
+% % than for other sources
+% 
+% strue=[3; 5; 7];
+% c=diag([3 1 1]);
+% 
+% sest=[1; 5; 9]
+% a=sest-strue;
+% a'*c*a
+% 
+% sest=[2; 6; 10]
+% a=sest-strue;
+% a'*c*a
+% 
+% % weight 2nd source more
+% c=diag([1 3 1]);
+% 
+% sest=[1; 5; 9]
+% a=sest-strue;
+% a'*c*a
+% 
+% sest=[2; 6; 10]
+% a=sest-strue;
+% a'*c*a
+% 
+
+
+%% apply to real data
+
 % What do we need to perform source localization with a minimum norm
 % method?
 % 1) Data
--- pinknoise.m
+++ pinknoise.m
@@ -1,6 +1,9 @@
-function x = pinknoise(Nx)
+function x = pinknoise(Nx,seed)
 % function x = pinknoise(Nx)
 
+if exist('seed','var')
+  ftFuncRandomseed=randomseed(seed);
+end
 B = [0.049922035 -0.095993537 0.050612699 -0.004408786];
 A = [1 -2.494956002   2.017265875  -0.522189400];
 nT60 = round(log(1000)/(1-max(abs(roots(A))))); % T60 est.
